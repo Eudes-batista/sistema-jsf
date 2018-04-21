c1cd88562f2c20887060f37eac0f25c2a374617f
zox <zox@servidor-primario>
2018-04-21 14:07:07 -0300

    foi criado um pacote so para armazenar os escopos , ciclo de vida dos objetos ViewScoped

--- src/java/com/zoomtecnologia/zox/modelo/cadastros/DocumentoPK.java
+++ src/java/com/zoomtecnologia/zox/modelo/cadastros/DocumentoPK.java
@@ -4,7 +4,6 @@ package com.zoomtecnologia.zox.modelo.cadastros;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.Data;
@@ -18,7 +17,6 @@ public class DocumentoPK implements Serializable{
     /**
      * CODIGO DO DOCUMENTO
      */
-    @Id
     @Length(max = 20, message = "campo documento do pode receber {max} caracteres")
     @Column(name = "DONUMERO", length = 20, nullable = false)
     String Codigo;
@@ -26,7 +24,6 @@ public class DocumentoPK implements Serializable{
     /**
      * CODIGO DA PESSOA
      */
-    @Id
     @ManyToOne
     @JoinColumn(name = "DOCODPES",nullable = false)        
     @ForeignKey(name = "documentoFKpessoa")
