a5be6aac9fec8302cc55d64d0fb6cd12699c7df8
zox <zox@servidor-primario>
2018-04-23 18:36:38 -0300

    ADICIONADO LOCAL DE ESTOQUE

--- src/java/com/zoomtecnologia/zox/modelo/cadastros/TipoContato.java
+++ src/java/com/zoomtecnologia/zox/modelo/cadastros/TipoContato.java
@@ -1,7 +1,16 @@
-
 package com.zoomtecnologia.zox.modelo.cadastros;
 
-
 public enum TipoContato {
-    TELEFONE,CELULAR,EMAIL,WHATSSAP,OUTROS;
+    TELEFONE("T"), CELULAR("C"), EMAIL("E"), WHATSSAP("W"), OUTROS("O");
+
+    private final String value;
+
+    private TipoContato(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
 }
