d28eb0e588029b548d49b55e66ac5a868d530ecb
Wagner <Wagner@Wagner-PC>
2018-10-16 15:23:37 -0300

    Proble de Exception

--- src/java/com/zoomtecnologia/zox/converter/ConverterData.java
+++ src/java/com/zoomtecnologia/zox/converter/ConverterData.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.zoomtecnologia.zox.converter;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.faces.component.UIComponent;
@@ -13,31 +7,28 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-/**
- *
- * @author Wagner
- */
 @FacesConverter("converterData")
 public class ConverterData implements Converter {
 
-    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        Date data = null;
         try {
             if (value != null) {
-                Date data = simpleDateFormat.parse(value);
+                data = simpleDateFormat.parse(value);
                 return data;
             }
-        } catch (ParseException ex) {
+        } finally {
+            return data;
         }
-        return null;
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
-        if(value != null){
-            if(value instanceof Date){
+        if (value != null) {
+            if (value instanceof Date) {
                 Date data = (Date) value;
                 return simpleDateFormat.format(data);
             }
