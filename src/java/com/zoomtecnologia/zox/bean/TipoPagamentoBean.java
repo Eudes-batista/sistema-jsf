31b31ed7c21879049044491e230f82e16f8bfc8a
Wagner <Wagner@Wagner-PC>
2019-02-11 17:54:23 -0300

    Adicionado A tela de tipo de forma de pagamento

---
+++ src/java/com/zoomtecnologia/zox/bean/TipoPagamentoBean.java
@@ -0,0 +1,29 @@
+package com.zoomtecnologia.zox.bean;
+
+import com.zoomtecnologia.zox.modelo.cadastros.TipoPagamento;
+import com.zoomtecnologia.zox.servico.TipoPagamentoService;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service("tipoPagamentoBean")
+@ManagedBean
+@Scope("view")
+public class TipoPagamentoBean extends GenericoBean<TipoPagamento, TipoPagamentoService> implements Serializable{
+
+    @Autowired
+    private TipoPagamentoService tipoPagamentoService;
+    
+    @Override
+    public TipoPagamentoService getGenericService() {
+        return this.tipoPagamentoService;
+    }
+
+    @Override
+    public TipoPagamento createEntidade() {
+        return new TipoPagamento();
+    }
+    
+}
--- src/java/com/zoomtecnologia/zox/servico/impl/TipoPagamentoServiceImpl.java
+++ src/java/com/zoomtecnologia/zox/servico/impl/TipoPagamentoServiceImpl.java
@@ -5,8 +5,13 @@ import com.zoomtecnologia.zox.servico.TipoPagamentoService;
 import java.io.Serializable;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +27,16 @@ public class TipoPagamentoServiceImpl extends GenericServiceImpl<TipoPagamento> 
     public Criteria criarCriteriaParaFiltro(TipoPagamento filtro) {
         Session session = entityManager.unwrap(Session.class);
         Criteria criteria = session.createCriteria(TipoPagamento.class);
+        if (!filtro.isFiltrar()) {
+            if (StringUtils.isNotEmpty(filtro.getPesquisa())) {
+                Criterion nome = Restrictions.ilike("descricao", filtro.getPesquisa(), MatchMode.ANYWHERE);
+                criteria.add(nome);
+                criteria.addOrder(Order.asc("descricao"));
+                return criteria;
+            }
+        } else {
+            return criarFiltro(filtro, criteria);
+        }
         return criteria;
     }
 
