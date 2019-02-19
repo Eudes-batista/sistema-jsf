84957bb83bef629bb1c77884ea7ddca4c283dd01
Wagner <Wagner@Wagner-PC>
2019-02-19 14:28:06 -0300

    Adicionado componente barra de botoes da grid

---
+++ src/java/com/zoomtecnologia/zox/bean/FormaPagamentoSaidaBean.java
@@ -0,0 +1,29 @@
+package com.zoomtecnologia.zox.bean;
+
+import com.zoomtecnologia.zox.modelo.cadastros.FormaPagamentoSaida;
+import com.zoomtecnologia.zox.servico.FormaPagamentoSaidaService;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service("formaPagamentoSaidaBean")
+@ManagedBean
+@Scope("view")
+public class FormaPagamentoSaidaBean extends GenericoBean<FormaPagamentoSaida, FormaPagamentoSaidaService> implements Serializable {
+
+    @Autowired
+    private FormaPagamentoSaidaService formaPagamentoSaidaService;
+
+    @Override
+    public FormaPagamentoSaidaService getGenericService() {
+        return this.formaPagamentoSaidaService;
+    }
+
+    @Override
+    public FormaPagamentoSaida createEntidade() {
+        return new FormaPagamentoSaida();
+    }
+
+}
